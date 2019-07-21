56a6c402a8e1d90d9459f018c9a74cfd77e2261c
Daniel Phillips <dphildev@gmail.com>
2019-07-21 13:53:06 +0100

    Removing old code- this has now been replaced by other classes.

--- src/main/java/Journal.java
+++
@@ -1,6 +0,0 @@
-import java.time.LocalDate;
-import java.util.HashMap;
-
-public class Journal {
-    private HashMap<LocalDate, Task> days = new HashMap<>();
-}
--- src/main/java/JournalApp.java
+++ src/main/java/JournalApp.java
@@ -9,18 +9,6 @@ import java.util.function.Supplier;
 
 public class JournalApp {
     public static void main(String[] args) {
-        /*
-        Task t = new Task("", false, 2);
-        t.setCompleted(true);
-        t.setDescription("Here's an updated task");
-        System.out.println(t);
-        System.out.println("hello!");
-        Database db = new Database("testfile.db");
-        db.updateTask(t);
-        Task restored = db.getTask(3);
-        System.out.println(restored);
-        */
-
         task.TaskDatabase db = new task.TaskDatabase("testtask.db");
         Supplier<ITaskSerialiser> createSerialiser = () -> new task.TaskSerialiser(db);
         task.Task t = new task.Task(createSerialiser);
